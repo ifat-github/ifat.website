9c3e90522d3bd09ce11ee0bea6e123af64ae9887
Ifat <ifat.email@gmail.com>
2019-09-24 13:10:28 +0300

    whole new website

--- src/components/CV.js
+++ src/components/CV.js
@@ -32,41 +32,41 @@ class CV extends Component {
                         this.state.displayCV ? (
                             <div>
                                 {/* CV */}
-                                <div class="cv">
-                                    <div class="container">
-                                        <div class="col-md-12">
-                                            <div id="cv" class="instaFade">
-                                                <div class="mainDetails">
-                                                    <div id="headshot" class="quickFade">
+                                <div className="cv">
+                                    <div className="container">
+                                        <div className="col-md-12">
+                                            <div id="cv" className="instaFade">
+                                                <div className="mainDetails">
+                                                    <div id="headshot" className="quickFade">
                                                         <img src={profile} alt="Ifat Ori" />
                                                     </div>
 
                                                     <div id="name">
-                                                        <h1 class="quickFade delayTwo">Ifat Ori</h1>
-                                                        <h2 class="quickFade delayThree">Software Developer</h2>
+                                                        <h1 className="quickFade delayTwo">Ifat Ori</h1>
+                                                        <h2 className="quickFade delayThree">Software Developer</h2>
                                                     </div>
 
-                                                    <div id="contactDetails" class="quickFade delayFour">
+                                                    <div id="contactDetails" className="quickFade delayFour">
                                                         <ul>
                                                             <li>e: <a href="mailto:ifat.email@gmail.com" target="_blank">ifat.email@gmail.com</a></li>
                                                             <li>w: <a href="http://ifat.website">ifat.website</a></li>
                                                             <li>m: 054-5968534</li>
                                                         </ul>
                                                     </div>
-                                                    <div class="clear"></div>
+                                                    <div className="clear"></div>
                                                 </div>
 
-                                                <div id="mainArea" class="quickFade delayFive">
+                                                <div id="mainArea" className="quickFade delayFive">
                                                     {/* Education */}
                                                     <section>
-                                                        <div class="sectionTitle">
+                                                        <div className="sectionTitle">
                                                             <h1>Education</h1>
                                                         </div>
 
-                                                        <div class="sectionContent">
+                                                        <div className="sectionContent">
                                                             <article>
                                                                 <h2>Java Developer at Infinity Labs R&D (Matrix)</h2>
-                                                                <p class="subDetails">2019</p>
+                                                                <p className="subDetails">2019</p>
                                                                 <p>Participated In a 6-months intense full-time & open lab training environment that covered a full
                                                                   range of development tools, technologies, and techniques.
                                                                   Have gained a total of over 1,200 coding hours.
@@ -79,20 +79,20 @@ class CV extends Component {
 
                                                             <article>
                                                                 <h2>Shenkar College</h2>
-                                                                <p class="subDetails">2012-2016 </p>
+                                                                <p className="subDetails">2012-2016 </p>
                                                                 <p>B.sc degree in Industrial Engineering, focus on Information Systems,</p>
                                                             </article>
                                                         </div>
-                                                        <div class="clear"></div>
+                                                        <div className="clear"></div>
                                                     </section>
                                                     {/* key Skills */}
                                                     <section>
-                                                        <div class="sectionTitle">
+                                                        <div className="sectionTitle">
                                                             <h1>Key Skills</h1>
                                                         </div>
 
-                                                        <div class="sectionContent">
-                                                            <ul class="keySkills">
+                                                        <div className="sectionContent">
+                                                            <ul className="keySkills">
                                                                 <li>Java</li>
                                                                 <li>C</li>
                                                                 <li>Linux</li>
@@ -104,18 +104,18 @@ class CV extends Component {
                                                                 <li>MySQL</li>
                                                             </ul>
                                                         </div>
-                                                        <div class="clear"></div>
+                                                        <div className="clear"></div>
                                                     </section>
                                                     {/* Work Experience */}
                                                     <section>
-                                                        <div class="sectionTitle">
+                                                        <div className="sectionTitle">
                                                             <h1>Work Experience</h1>
                                                         </div>
 
-                                                        <div class="sectionContent">
+                                                        <div className="sectionContent">
                                                             <article>
                                                                 <h2>Marketing Automation Specialist at eToro</h2>
-                                                                <p class="subDetails">July 2016 - August 2018</p>
+                                                                <p className="subDetails">July 2016 - August 2018</p>
                                                                 <p>Created emails with HTML, CSS
                                                                   Managed and modified an automated PHP system
                                                                   Found solutions for optimizing and improving internal processes
@@ -126,34 +126,34 @@ class CV extends Component {
 
                                                             <article>
                                                                 <h2>Referent in "Kalanet" Project at NTA (The Israeli Light Rail)</h2>
-                                                                <p class="subDetails">2015 - 2016</p>
+                                                                <p className="subDetails">2015 - 2016</p>
                                                             </article>
                                                         </div>
-                                                        <div class="clear"></div>
+                                                        <div className="clear"></div>
                                                     </section>
                                                     {/* Languages */}
                                                     <section>
-                                                        <div class="sectionTitle">
+                                                        <div className="sectionTitle">
                                                             <h1>Languages</h1>
                                                         </div>
 
-                                                        <div class="sectionContent">
-                                                            <ul class="keySkills">
+                                                        <div className="sectionContent">
+                                                            <ul className="keySkills">
                                                                 <li>
                                                                     <article>
                                                                         <h2>Hebrew</h2>
-                                                                        <p class="subDetails">Mother Tongue</p>
+                                                                        <p className="subDetails">Mother Tongue</p>
                                                                     </article>
                                                                 </li>
                                                                 <li>
                                                                     <article>
                                                                         <h2>English</h2>
-                                                                        <p class="subDetails">Excellent level</p>
+                                                                        <p className="subDetails">Excellent level</p>
                                                                     </article>
                                                                 </li>
                                                             </ul>
                                                         </div>
-                                                        <div class="clear"></div>
+                                                        <div className="clear"></div>
                                                     </section>
                                                 </div>
                                             </div>
