d871f9e3b0c444840ef452e347047dabf84ae95c
Ifat <ifat.email@gmail.com>
2019-09-26 18:26:23 +0300

    mobile bg

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -30744,7 +30744,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38773" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36825" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
